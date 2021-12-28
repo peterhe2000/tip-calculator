80eb1cc23fbfd50f4e6e175c89cad22ecf1ef74a
Peter He <peter@mathspathway.com>
2021-12-28 18:02:00 +1100

    added bindActionCreators to NewItemFormContainer

--- src/containers/NewItemFormContainer.js
+++ src/containers/NewItemFormContainer.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import NewItemForm from '../components/NewItemForm';
 import { addNewItem } from '../store/items/actions';
+import { bindActionCreators } from 'redux';
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    onSubmit: (name, price) => dispatch(addNewItem(name, price))
-  };
+  return bindActionCreators(
+    {
+      onSubmit: (name, price) => addNewItem(name, price)
+    }, dispatch);
 };
 
 // a connect without argument is going pass in empty object for state props and then it will actually pass in dispatch as a prop to that object
