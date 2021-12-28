9b02f03d7269dfb5f6b2f53a3342aa6f16ffce1c
Peter He <peter@mathspathway.com>
2021-12-28 18:05:26 +1100

    if u do mapDispatchToProps with an object without argument, it will automatically map dispatch to props for u. For simple map case

--- src/containers/NewItemFormContainer.js
+++ src/containers/NewItemFormContainer.js
@@ -1,13 +1,10 @@
 import { connect } from 'react-redux';
 import NewItemForm from '../components/NewItemForm';
 import { addNewItem } from '../store/items/actions';
-import { bindActionCreators } from 'redux';
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(
-    {
-      onSubmit: (name, price) => addNewItem(name, price)
-    }, dispatch);
+// if u do mapDispatchToProps with an object without argument, it will automatically map dispatch to props for u.
+const mapDispatchToProps = {  
+  onSubmit: (name, price) => addNewItem(name, price)
 };
 
 // a connect without argument is going pass in empty object for state props and then it will actually pass in dispatch as a prop to that object
