80c0038bf10ac5f3748b05c65dd130a229918106
Peter He <peter@mathspathway.com>
2021-12-29 11:33:16 +1100

    refactor reducer to use immer

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,4 +1,5 @@
 import { ITEM_ADDED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED, ITEM_REMOVED } from './actions';
+import produce from 'immer';
 
 let id = 1;
 
@@ -7,36 +8,31 @@ export const initialItems = [
   { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
 ];
 
-export const reducer = (state = initialItems, action) => {
-  if(action.type === ITEM_ADDED) {
+export const reducer = produce((state = initialItems, action) => {
+  if (action.type === ITEM_ADDED) {
     const item = {
-      uuid: id ++,
+      uuid: id++,
       quantity: 1,
-      ...action.payload
-    }
-    return [...state, item];
+      name: action.payload.name,
+      price: parseInt(action.payload.price, 10)
+    };
+
+    state.push(item);
   }
 
   if (action.type === ITEM_REMOVED) {
-    return state.filter(item => item.uuid !== action.payload.uuid);
+    return state.filter((item) => item.uuid !== action.payload.uuid);
   }
 
-  if(action.type === ITEM_PRICE_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid !== action.payload.uuid) return item;
-      return { ...item, price: action.payload.price };
-    });
+  if (action.type === ITEM_PRICE_UPDATED) {
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.price = parseInt(action.payload.price, 10);
   }
 
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map((item) => {
-      if (item.uuid !== action.payload.uuid) return item;
-      return { ...item, quantity: action.payload.quantity };
-    });
+    const item = state.find((item) => item.uuid === action.payload.uuid);
+    item.quantity = parseInt(action.payload.quantity, 10);
   }
-
-  return state;
-};
-
+}, initialItems);
 
 export default reducer;
